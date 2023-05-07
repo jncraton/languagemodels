c3ce5645c14af9961c59ee7c2fe329421d24fa8b
Jon Craton <jncraton@gmail.com>
2023-05-06 21:51:25 -0400

    Reformat with black

--- languagemodels/languagemodels.py
+++ languagemodels/languagemodels.py
@@ -1,17 +1,19 @@
 class LLM:
-    def __init__(self, engine='transformers', model='google/flan-t5-base'):
-        if engine == 'transformers':
+    def __init__(self, engine="transformers", model="google/flan-t5-base"):
+        if engine == "transformers":
             from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
             model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
             tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
 
             def generate(prompt, tokens=128):
                 inputs = tokenizer(prompt, return_tensors="pt")
                 outputs = model.generate(**inputs, max_new_tokens=tokens)
-                return(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+                return tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
             self.generate = generate
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     llm = LLM()
     print(llm.generate("How do I make Ramen noodles?"))
