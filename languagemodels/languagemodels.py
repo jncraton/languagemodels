73b0a77108e87544a3bef44c453dd789e394ed7c
Jon Craton <jncraton@gmail.com>
2023-05-07 09:33:51 -0400

    Load models just-in-time and cache them

--- languagemodels/languagemodels.py
+++ languagemodels/languagemodels.py
@@ -1,10 +1,27 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
-tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+modelcache = {}
+tokenizercache = {}
+
+
+def get_model(model):
+    if model not in modelcache:
+        modelcache[model] = AutoModelForSeq2SeqLM.from_pretrained(model)
+
+    return modelcache[model]
+
+
+def get_tokenizer(tokenizer):
+    if tokenizer not in tokenizercache:
+        tokenizercache[tokenizer] = AutoTokenizer.from_pretrained(tokenizer)
+
+    return tokenizercache[tokenizer]
 
 
 def chat(userprompt):
+    model = get_model("google/flan-t5-base")
+    tokenizer = get_tokenizer("google/flan-t5-base")
+
     prompt = (
         f"System: "
         f"Agent responses will be truthful, helpful, and harmless.\n"
