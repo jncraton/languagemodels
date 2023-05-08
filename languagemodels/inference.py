dabf70b853ca681ab007618d20f644044be24d87
Jon Craton <jncraton@gmail.com>
2023-05-08 19:34:14 -0400

    Remove unused code

--- languagemodels/inference.py
+++ languagemodels/inference.py
@@ -1,6 +1,6 @@
 import requests
 import os
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+from transformers import pipeline
 
 
 class InferenceException(Exception):
@@ -8,23 +8,6 @@ class InferenceException(Exception):
 
 
 modelcache = {}
-tokenizercache = {}
-
-
-def get_model(model):
-    if model not in modelcache:
-        modelcache[model] = AutoModelForSeq2SeqLM.from_pretrained(
-            model, low_cpu_mem_usage=True
-        )
-
-    return modelcache[model]
-
-
-def get_tokenizer(tokenizer):
-    if tokenizer not in tokenizercache:
-        tokenizercache[tokenizer] = AutoTokenizer.from_pretrained(tokenizer)
-
-    return tokenizercache[tokenizer]
 
 
 def generate_instruct(prompt, max_tokens=200):
