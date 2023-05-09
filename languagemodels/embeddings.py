5eea1ed300e2992d4827b5cfb18d9da167c179e3
Jon Craton <jncraton@gmail.com>
2023-05-09 15:10:23 -0400

    Create RetrievalContext

--- languagemodels/embeddings.py
+++ languagemodels/embeddings.py
@@ -1,4 +1,5 @@
 from sentence_transformers import SentenceTransformer, util
+import numpy
 
 model = None
 
@@ -33,3 +34,50 @@ def get_dot_scores(query, docs):
 
     doc_score_pairs = sorted(doc_score_pairs, key=lambda x: x[1], reverse=True)
     return doc_score_pairs
+
+
+class RetrievalContext:
+    """
+    Provides a context for document retrieval
+
+    Documents are embedded and cached for later search.
+
+    Example usage:
+
+    >>> rc = RetrievalContext()
+    >>> rc.store("The sky is blue.")
+    >>> rc.store("Paris is in France.")
+    >>> rc.store("Mars is a planet.")
+    >>> rc.get_match("Where is Paris?")
+    'Paris is in France.'
+
+    >>> rc.clear()
+    >>> rc.get_match("Where is Paris?")
+    """
+
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self.docs = []
+        self.embeddings = None
+
+    def store(self, doc):
+        if doc not in self.docs:
+            self.docs.append(doc)
+            embedding = encode([doc])
+            if isinstance(self.embeddings, numpy.ndarray):
+                self.embeddings = numpy.concatenate((self.embeddings, embedding))
+            else:
+                self.embeddings = embedding
+
+    def get_match(self, query):
+        if len(self.docs) == 0:
+            return None
+
+        scores = util.dot_score(encode([query])[0], self.embeddings)[0].cpu().tolist()
+
+        doc_score_pairs = list(zip(self.docs, scores))
+
+        doc_score_pairs = sorted(doc_score_pairs, key=lambda x: x[1], reverse=True)
+        return doc_score_pairs[0][0]
