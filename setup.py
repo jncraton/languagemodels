b4dd1e3f3376faa69b85ec2f24a518837dc97294
Jon Craton <jncraton@gmail.com>
2023-05-12 12:19:21 -0400

    Rev version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="languagemodels",
-    version="0.0.2",
+    version="0.0.3",
     author="Jon Craton",
     author_email="jon@joncraton.com",
     description="Simple inference for large language models",
