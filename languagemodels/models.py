2cc561d8b03ef80ea8b1166bdf4f51f5ae1a1bf6
Jon Craton <jncraton@gmail.com>
2023-07-08 11:46:29 -0400

    Remove unused conversion function

--- languagemodels/models.py
+++ languagemodels/models.py
@@ -68,46 +68,6 @@ def require_model_license(match_re):
     config["model_license"] = match_re
 
 
-def convert_to_gb(space):
-    """Convert max RAM string to int
-
-    Output will be in gigabytes
-
-    If not specified, input is assumed to be in gigabytes
-
-    >>> convert_to_gb("512")
-    512.0
-
-    >>> convert_to_gb(".5")
-    0.5
-
-    >>> convert_to_gb("4G")
-    4.0
-
-    >>> convert_to_gb("256mb")
-    0.25
-
-    >>> convert_to_gb("256M")
-    0.25
-    """
-
-    if isinstance(space, int) or isinstance(space, float):
-        return float(space)
-
-    multipliers = {
-        "g": 1.0,
-        "m": 2 ** -10,
-    }
-
-    space = space.lower()
-    space = space.rstrip("b")
-
-    if space[-1] in multipliers:
-        return float(space[:-1]) * multipliers[space[-1]]
-    else:
-        return float(space)
-
-
 def get_model_info(model_type="instruct", max_ram=None, license_match=None):
     """Gets info about the current model in use
 
