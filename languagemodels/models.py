ea1ab3b4fdf6c341726b47de21d978e935b3a2ae
Jon Craton <jncraton@gmail.com>
2023-07-08 23:29:15 -0400

    Add support for e5 small embeddings

--- languagemodels/models.py
+++ languagemodels/models.py
@@ -70,7 +70,7 @@ def get_model(model_type, tokenizer_only=False):
         tok_config = hf_hub_download(f"jncraton/{model_name}", "tokenizer.json")
         tokenizer = Tokenizer.from_file(tok_config)
 
-        if "minilm" in model_name.lower():
+        if model_type == "embedding":
             tokenizer.no_padding()
             tokenizer.no_truncation()
 
