7daaa35b50e7aff1af71ee147efd41d63fd059c8
Jon Craton <jncraton@gmail.com>
2023-07-10 15:17:59 -0400

    Fix quotes

--- languagemodels/models.py
+++ languagemodels/models.py
@@ -70,7 +70,7 @@ def get_model(model_type, tokenizer_only=False):
         tok_config = hf_hub_download(f"jncraton/{model_name}", "tokenizer.json")
         tokenizer = Tokenizer.from_file(tok_config)
 
-        if model_type == 'embedding':
+        if model_type == "embedding":
             tokenizer.no_padding()
             tokenizer.no_truncation()
 
