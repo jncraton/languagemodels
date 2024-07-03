335b1dfd4084e7bc2e8ea859187c7d0c6403a47e
Jon Craton <jncraton@gmail.com>
2024-07-03 16:38:40 -0400

    Add latest Phi3-mini

--- languagemodels/config.py
+++ languagemodels/config.py
@@ -226,6 +226,18 @@ models = [
         "license": "apache-2.0",
     },
     {
+        "name": "Phi-3-mini-4k-instruct-20240701",
+        "tuning": "instruct",
+        "datasets": ["phi-3"],
+        "params": 3.8e9,
+        "quantization": "int8",
+        "backend": "ct2",
+        "architecture": "decoder-only-transformer",
+        "license": "mit",
+        "prompt_fmt": "<|user|>\n{instruction}<|end|>\n<|assistant|>",
+        "repetition_penalty": 1.1,
+    },
+    {
         "name": "Phi-3-mini-4k-instruct",
         "tuning": "instruct",
         "datasets": ["phi-3"],
